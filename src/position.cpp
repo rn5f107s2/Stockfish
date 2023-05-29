cb87f6de1188f772f3cba1958cbcf31715c78c5c
rn5f107s2 <clemens.lerchl@gmail.com>
2023-05-29 19:10:08 +0200

    Replaced MagicBB Look up with betweenBB

--- src/position.cpp
+++ src/position.cpp
@@ -669,7 +669,7 @@ bool Position::gives_check(Move m) const {
       Square rto = relative_square(sideToMove, to > from ? SQ_F1 : SQ_D1);
 
       return   (attacks_bb<ROOK>(rto) & ksq)
-            && (attacks_bb<ROOK>(rto, pieces() ^ from ^ to) & ksq);
+            && !(between_bb(ksq, rto) & (pieces() ^ from ^ to));
   }
   }
 }
