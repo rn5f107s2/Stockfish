6f09d2a030941a10394d168e157dfb7b8157afda
rn5f107s2 <clemens.lerchl@gmail.com>
2023-06-13 22:05:13 +0200

    reduce more for likely fail lows

--- src/search.cpp
+++ src/search.cpp
@@ -1164,6 +1164,9 @@ moves_loop: // When in check, search starts here
       if (singularQuietLMR)
           r--;
 
+      if (likelyFailLow)
+          r++;
+
       // Increase reduction if next ply has a lot of fail high (~5 Elo)
       if ((ss+1)->cutoffCnt > 3)
           r++;
