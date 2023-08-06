9572b034e0e62658000c34857b6503867360fb5a
rn5f107s2 <clemens.lerchl@gmail.com>
2023-08-06 13:24:16 +0200

    bench 1631673

--- src/search.cpp
+++ src/search.cpp
@@ -836,7 +836,7 @@ namespace {
         && !ttMove)
         depth -= 2;
 
-    probCutBeta = beta + 168 - 61 * improving;
+    probCutBeta = beta + 168 - 61 * (improving || (ttCapture && ttValue >= beta));
 
     // Step 11. ProbCut (~10 Elo)
     // If we have a good enough capture (or queen promotion) and a reduced search returns a value
