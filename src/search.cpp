fea88944c806b455241384364e9c49e7cc423ca4
rn5f107s2 <clemens.lerchl@gmail.com>
2023-08-12 21:33:35 +0200

    In PvNodes only movecountprune in nodes where the sideToMove is the same as in the root bench 1485436

--- src/search.cpp
+++ src/search.cpp
@@ -973,7 +973,7 @@ moves_loop: // When in check, search starts here
           && bestValue > VALUE_TB_LOSS_IN_MAX_PLY)
       {
           // Skip quiet moves if movecount exceeds our FutilityMoveCount threshold (~8 Elo)
-          moveCountPruning = moveCount >= futility_move_count(improving, depth);
+          moveCountPruning = (!PvNode || (ss->ply % 2 == 0)) && moveCount >= futility_move_count(improving, depth);
 
           // Reduced depth of the next LMR search
           int lmrDepth = newDepth - r;
