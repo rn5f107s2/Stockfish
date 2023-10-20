a9b3a93de6d79967e9f290278a9a463410255e64
rn5f107s2 <clemens.lerchl@gmail.com>
2023-10-20 14:57:03 +0200

    Prune all non queen promos in qsearch, - even if they are captures bench 1268400

--- src/search.cpp
+++ src/search.cpp
@@ -1544,6 +1544,9 @@ moves_loop: // When in check, search starts here
         // Step 6. Pruning.
         if (bestValue > VALUE_TB_LOSS_IN_MAX_PLY && pos.non_pawn_material(us))
         {
+            if (type_of(move) == PROMOTION && promotion_type(move) != QUEEN)
+                continue;
+
             // Futility pruning and moveCount pruning (~10 Elo)
             if (   !givesCheck
                 &&  to_sq(move) != prevSq
