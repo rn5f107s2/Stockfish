ae7baa7810dda41c7c498a51e8206f9e0666c0ff
rn5f107s2 <clemens.lerchl@gmail.com>
2023-11-04 07:51:39 +0100

    Retry this as there is now twice the amount of histories getting updated twice bench 1148825

--- src/search.cpp
+++ src/search.cpp
@@ -741,7 +741,7 @@ Value search(Position& pos, Stack* ss, Value alpha, Value beta, Depth depth, boo
     }
 
     // Use static evaluation difference to improve quiet move ordering (~4 Elo)
-    if (is_ok((ss - 1)->currentMove) && !(ss - 1)->inCheck && !priorCapture)
+    if (is_ok((ss - 1)->currentMove) && !(ss - 1)->inCheck && !priorCapture && !excludedMove)
     {
         int bonus = std::clamp(-18 * int((ss - 1)->staticEval + ss->staticEval), -1812, 1812);
         thisThread->mainHistory[~us][from_to((ss - 1)->currentMove)] << bonus;
