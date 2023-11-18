1265b6485117df217c7a64ae4a22a9d1d75f77d4
rn5f107s2 <clemens.lerchl@gmail.com>
2023-11-18 11:53:56 +0100

    Allow negative redutcions for the ttMove instead of always setting them to zero bench 1410021

--- src/search.cpp
+++ src/search.cpp
@@ -1158,7 +1158,7 @@ moves_loop:  // When in check, search starts here
         // Set reduction to 0 for first generated move (ttMove)
         // Nullifies all previous reduction adjustments to ttMove and leaves only history to do them
         else if (move == ttMove)
-            r = 0;
+            r = std::min(0, r);
 
         ss->statScore = 2 * thisThread->mainHistory[us][from_to(move)]
                       + (*contHist[0])[movedPiece][to_sq(move)]
