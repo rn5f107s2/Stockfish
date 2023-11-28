e58e87cd5c8d39c63d1bd0987538877cc6c57015
rn5f107s2 <clemens.lerchl@gmail.com>
2023-11-28 18:39:48 +0100

    If r50c is below 27 scale eval down as if it was 27 bench 1372484

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -185,6 +185,9 @@ Value Eval::evaluate(const Position& pos) {
         v       = (nnue * (915 + npm + 9 * pos.count<PAWN>()) + optimism * (154 + npm)) / 1024;
     }
 
+    if (shuffling < 27)
+        shuffling = 0;
+
     // Damp down the evaluation linearly when shuffling
     v = v * (200 - shuffling) / 214;
 
