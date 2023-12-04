7647ff437cdd8cb034f2670b67c89f6ff910a259
rn5f107s2 <clemens.lerchl@gmail.com>
2023-12-04 15:05:04 +0100

    !PvNode bench 1365060

--- src/search.cpp
+++ src/search.cpp
@@ -969,15 +969,16 @@ moves_loop:  // When in check, search starts here
         // Depth conditions are important for mate finding.
         if (!rootNode && pos.non_pawn_material(us) && bestValue > VALUE_TB_LOSS_IN_MAX_PLY)
         {
-            // Skip quiet moves if movecount exceeds our FutilityMoveCount threshold (~8 Elo)
-            if (!moveCountPruning)
-                moveCountPruning = moveCount >= futility_move_count(improving, depth);
-
-            if (   ss->inCheck
+            if (   !PvNode
+                && ss->inCheck
                 && moveCountPruning
                 && !capture
                 && !givesCheck)
                 continue;
+
+            // Skip quiet moves if movecount exceeds our FutilityMoveCount threshold (~8 Elo)
+            if (!moveCountPruning)
+                moveCountPruning = moveCount >= futility_move_count(improving, depth);
 
             // Reduced depth of the next LMR search
             int lmrDepth = newDepth - r;
