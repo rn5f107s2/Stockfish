0f13e6965a33bf15556dac8bb10750293c99cc6b
rn5f107s2 <clemens.lerchl@gmail.com>
2024-01-13 22:35:52 +0100

    Dont update any stats in check bench 1299892

--- src/search.cpp
+++ src/search.cpp
@@ -1370,9 +1370,11 @@ moves_loop:  // When in check, search starts here
 
     // If there is a move that produces search value greater than alpha we update the stats of searched moves
     else if (bestMove)
-        update_all_stats(pos, ss, *this, bestMove, bestValue, beta, prevSq, quietsSearched,
-                         quietCount, capturesSearched, captureCount, depth);
-
+    {
+        if (!ss->inCheck)
+            update_all_stats(pos, ss, *this, bestMove, bestValue, beta, prevSq, quietsSearched,
+                             quietCount, capturesSearched, captureCount, depth);
+    }
     // Bonus for prior countermove that caused the fail low
     else if (!priorCapture && prevSq != SQ_NONE)
     {
