ed054e14acb58e8dd7cb105bab5ed178f4942129
rn5f107s2 <clemens.lerchl@gmail.com>
2024-01-14 08:38:03 +0100

    Sort the contermove before all other quiets in check bench 1144109

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -214,10 +214,15 @@ void MovePicker::score() {
             if (pos.capture_stage(m))
                 m.value =
                   PieceValue[pos.piece_on(m.to_sq())] - type_of(pos.moved_piece(m)) + (1 << 28);
-            else
+            else 
+            {
                 m.value = (*mainHistory)[pos.side_to_move()][m.from_to()]
                         + (*continuationHistory[0])[pos.moved_piece(m)][m.to_sq()]
                         + (*pawnHistory)[pawn_structure_index(pos)][pos.moved_piece(m)][m.to_sq()];
+
+                if (m == refutations[2])
+                    m.value += (1 << 20);
+            }
         }
 }
 
