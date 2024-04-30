07eec601a7da9ddb11c4da89e5070d39932aa654
rn5f107s2 <clemens.lerchl@gmail.com>
2024-04-30 07:49:57 +0200

    Take 7, dont let the ttMove get move loop pruned bench 1553527

--- src/search.cpp
+++ src/search.cpp
@@ -956,7 +956,7 @@ moves_loop:  // When in check, search starts here
 
         // Step 14. Pruning at shallow depth (~120 Elo).
         // Depth conditions are important for mate finding.
-        if (!rootNode && pos.non_pawn_material(us) && bestValue > VALUE_TB_LOSS_IN_MAX_PLY)
+        if (!rootNode && pos.non_pawn_material(us) && bestValue > VALUE_TB_LOSS_IN_MAX_PLY && move != ttMove)
         {
             // Skip quiet moves if movecount exceeds our FutilityMoveCount threshold (~8 Elo)
             if (!moveCountPruning)
