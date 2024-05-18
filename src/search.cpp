77427ec73072d552e52ce2d1bb2914d195ed0ae6
rn5f107s2 <clemens.lerchl@gmail.com>
2024-05-18 13:30:47 +0200

    Retry this with the killer fix bench 1144699

--- src/search.cpp
+++ src/search.cpp
@@ -1285,7 +1285,7 @@ moves_loop:  // When in check, search starts here
 
             if (value > alpha)
             {
-                if constexpr (rootNode)
+                if (rootNode && bestMove)
                     update_refutations(pos, ss, *this, bestMove);
 
                 bestMove = move;
