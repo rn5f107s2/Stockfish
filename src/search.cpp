9987a93c74ff997658f296ebc3324ed69c19b3f2
rn5f107s2 <clemens.lerchl@gmail.com>
2024-07-13 23:28:02 +0200

    Dont allow nmp for the first 3 plies bench 1528836

--- src/search.cpp
+++ src/search.cpp
@@ -795,7 +795,7 @@ Value Search::Worker::search(
 
     // Step 9. Null move search with verification search (~35 Elo)
     if (!PvNode && (ss - 1)->currentMove != Move::null() && (ss - 1)->statScore < 14389
-        && eval >= beta && ss->staticEval >= beta - 21 * depth + 390 && !excludedMove
+        && eval >= beta && ss->ply >= 3 && ss->staticEval >= beta - 21 * depth + 390 && !excludedMove
         && pos.non_pawn_material(us) && ss->ply >= thisThread->nmpMinPly
         && beta > VALUE_TB_LOSS_IN_MAX_PLY)
     {
