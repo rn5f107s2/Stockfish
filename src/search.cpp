b75e86c17bccdf84a14c3180182cd329c0f5a33b
rn5f107s2 <clemens.lerchl@gmail.com>
2024-08-21 18:33:37 +0200

    Retry this with the idea by martinn (motor author) to or doDeeper and nightmare, the main difference between doDeeper and nightmare is, that doDeeper would trigger if bestValue is slightly worse than alpha, but value a lot better, while nightmare triggers a lot more agressively when bestValue is bad bench 1341629

--- src/search.cpp
+++ src/search.cpp
@@ -1172,10 +1172,11 @@ moves_loop:  // When in check, search starts here
             {
                 // Adjust full-depth search based on LMR results - if the result was
                 // good enough search deeper, if it was bad enough search shallower.
+                const bool nightmare         = bestValue < alpha - 25;
                 const bool doDeeperSearch    = value > (bestValue + 35 + 2 * newDepth);  // (~1 Elo)
                 const bool doShallowerSearch = value < bestValue + 8;                    // (~2 Elo)
 
-                newDepth += doDeeperSearch - doShallowerSearch;
+                newDepth += (doDeeperSearch || nightmare) - doShallowerSearch;
 
                 if (newDepth > d)
                     value = -search<NonPV>(pos, ss + 1, -(alpha + 1), -alpha, newDepth, !cutNode);
