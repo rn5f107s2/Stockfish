fbbc1cc9a972db5ff2f63443b2d4ea1fd4af620b
rn5f107s2 <clemens.lerchl@gmail.com>
2025-10-19 12:17:19 +0200

    Second fused

--- src/nnue/nnue_accumulator.cpp
+++ src/nnue/nnue_accumulator.cpp
@@ -208,7 +208,7 @@ void AccumulatorStack::forward_update_incremental(
             DirtyPiece& dp1 = psq_accumulators[next].diff;
             DirtyPiece& dp2 = psq_accumulators[next + 1].diff;
 
-            if (std::is_same_v<FeatureSet, ThreatFeatureSet> && dp2.remove_sq != SQ_NONE && (threat_accumulators[next].diff.threateningSqs & square_bb(dp2.remove_sq))) 
+            if (std::is_same_v<FeatureSet, ThreatFeatureSet> && dp2.remove_sq != SQ_NONE && ((threat_accumulators[next].diff.threateningSqs & square_bb(dp2.remove_sq)) || (threat_accumulators[next].diff.threatenedSqs & square_bb(dp2.remove_sq)))) 
             {
                 double_inc_update<Perspective>(featureTransformer, ksq, threat_accumulators[next], threat_accumulators[next + 1], threat_accumulators[next - 1], dp2);
                 next++;
