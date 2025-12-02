df25635807753b7060256269dd3640e741f0a98e
rn5f107s2 <clemens.lerchl@gmail.com>
2025-12-02 20:38:45 +0100

    Before hindsight reductions were not done in check, see if there is any gain in reverting it to the old way bench 2572179

--- src/search.cpp
+++ src/search.cpp
@@ -747,9 +747,9 @@ Value Search::Worker::search(
     opponentWorsening = ss->staticEval > -(ss - 1)->staticEval;
 
     // Hindsight adjustment of reductions based on static evaluation difference.
-    if (priorReduction >= 3 && !opponentWorsening)
+    if (!ss->inCheck && priorReduction >= 3 && !opponentWorsening)
         depth++;
-    if (priorReduction >= 2 && depth >= 2 && ss->staticEval + (ss - 1)->staticEval > 173)
+    if (!ss->inCheck && priorReduction >= 2 && depth >= 2 && ss->staticEval + (ss - 1)->staticEval > 173)
         depth--;
 
     // At non-PV nodes we check for an early TT cutoff
